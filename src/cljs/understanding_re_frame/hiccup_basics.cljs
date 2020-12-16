5407c0f31c679c8f2a97c8e219cc38c26d27bdc6
matheusfrancisco <matheusmachadoufsc@gmail.com>
2020-12-16 08:51:16 -0300

    understanding hiccup events reagent

--- src/cljs/understanding_re_frame/hiccup_basics.cljs
+++ src/cljs/understanding_re_frame/hiccup_basics.cljs
@@ -11,18 +11,36 @@
   (let [title "Hello"
         border-color "green"
         active? false]
-    [:div {:class (cs "content"
-                       "row "
-                       (when active?
-                         "active"))
+    [:div#main-content.content.row {:id "main-content"
+           :class (when active?
+                         "active")
            :style {:padding-left "10%"
                    :border (when border-color
                              (str "4px solid " border-color))}}
-     [:h1 "Hiccup"]
+     [:h1#hiccup.big "Hiccup"]
+     [:div.wrapper
+      {:style {:width 100}
+       :on-click (fn [e]
+                   (js/console.log "Wrapper Dive"))}
+      [:div.container
+       [:div.inner
+        {:on-click (fn [e]
+                     (.stopPropagation e)
+                     (js/console.log "Inner div")
+                     (js/console.log (.-target e)))}
+        [:p "some content"]
+        [:p "other content"]]]]
+     [:div.wrapper>div.container>di.inner
+        [:p "some content"]
+        [:p "other content"]]
      [:p "This is a message. "]
      [:input {:type :checkbox
               :checked true}]
      [:p "another paragraph."]
      [:input {:type :email }]
-     [:p [:a {:href "#/"} "Home"]]
+     [:p [:a {:href "#/"
+              :on-click (fn [e]
+                          (.preventDefault e)
+                          (.stopPropagation e)
+                          (js/console.log "Link"))} "Home"]]
      [:p "Some more. " [:b "And this is bold"]]]))
